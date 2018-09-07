151674bc948a7d79d889b635667341c498c562b4
John A. De Goes <john@degoes.net>
2018-09-07 09:05:04 +0200

    update exercises

--- src/main/scala/net/degoes/02-abstractions/exercises.scala
+++ src/main/scala/net/degoes/02-abstractions/exercises.scala
@@ -63,7 +63,34 @@ object functor {
   implicit val DataTypeFunctor: Functor[DataType] = ???
 
   //
-  // EXERCUSE 5
+  // EXERCISE 5
+  //
+  // Define an instance of `Functor` for `FunctorProduct`.
+  //
+  case class FunctorProduct[F[_], G[_], A](l: F[A], r: F[A])
+  implicit def FunctorProductFunctor[F[_]: Functor, G[_]: Functor]:
+    Functor[FunctorProduct[F, G, ?]] = ???
+
+  //
+  // EXERCISE 6
+  //
+  // Define an instance of `Functor` for `FunctorSum`.
+  //
+  case class FunctorSum[F[_], G[_], A](run: Either[F[A], G[A]])
+  implicit def FunctorSumFunctor[F[_]: Functor, G[_]: Functor]:
+    Functor[FunctorSum[F, G, ?]] = ???
+
+  //
+  // EXERCISE 7
+  //
+  // Define an instance of `Functor` for `FunctorNest`.
+  //
+  case class FunctorNest[F[_], G[_], A](run: F[G[A]])
+  implicit def FunctorNestFunctor[F[_]: Functor, G[_]: Functor]:
+    Functor[FunctorNest[F, G, ?]] = ???
+
+  //
+  // EXERCUSE 8
   //
   // Implement `zip` in terms of the applicative composition using `|@|`.
   //
@@ -72,21 +99,21 @@ object functor {
   def zip[F[_]: Applicative, A, B](l: F[A], r: F[B]): F[(A, B)] = ???
 
   //
-  // EXERCISE 6
+  // EXERCISE 9
   //
   // Define an instance of `Applicative` for `Parser[E, ?]`.
   //
   implicit def ApplicativeParser[E]: Applicative[Parser[E, ?]] = ???
 
   //
-  // EXERCISE 8
+  // EXERCISE 10
   //
   // Define an instance of `Monad` for `BTree`.
   //
   implicit val MonadBTree: Monad[BTree] = ???
 
   //
-  // EXERCISE 9
+  // EXERCISE 11
   //
   // Define an instance of `Monad` for `Parser[E, ?]`.
   //
@@ -102,6 +129,28 @@ object foldable {
   sealed trait BTree[+A]
   case class Leaf[A](a: A) extends BTree[A]
   case class Fork[A](left: BTree[A], right: BTree[A]) extends BTree[A]
+
+  //
+  // EXERCISE 2
+  //
+  // Define an instance of `Foldable` for `A => ?`.
+  //
+  implicit def FunctionFoldable[A]: Foldable[A => ?] = ???
+
+  //
+  // EXERCISE 3
+  //
+  // Define an instance of `Traverse` for `BTree`.
+  //
+  implicit val TraverseBTree: Traverse[BTree] = ???
+
+  //
+  // EXERCISE 4
+  //
+  // Define an instance of `Traverse` for `Parser[E, ?]`.
+  //
+  case class Parser[+E, +A](run: String => Either[E, (String, A)])
+  implicit def TraverseParser[E]: Traverse[Parser[E, ?]] = ???
 }
 
 object optics {
